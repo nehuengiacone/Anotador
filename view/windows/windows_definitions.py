cab20b42e0e8a081a4a70178f1b716d0c94e191b
Nehuen Giacone <nehuengiacone@gmail.com>
2023-09-30 03:12:24 -0300

    Colores en las ventanas. Icono en esquina de ventana

--- view/windows/windows_definitions.py
+++ view/windows/windows_definitions.py
@@ -6,13 +6,13 @@ from classes import Ventana
 
 # INSTANCIAS DE VENTANA
 #instancia de ventana principal
-root = Ventana.Ventana(width=400, height=300, titulo="Anotador - Login", root=True, completa=False, maximiza=False)
-main = Ventana.Ventana(width=400, height=300, titulo="Anotador - Main", root=True, completa=False, maximiza=False)
-crear_nota = Ventana.Ventana(width=400, height=600, titulo="Anotador - Crear Nota", root=False, completa=False, maximiza=False)
-modificar_nota = Ventana.Ventana(width=400, height=600, titulo="Anotador - Modificar Nota", root=False, completa=False, maximiza=False)
-nota = Ventana.Ventana(width=330, height=330, titulo="Anotador - Nota", root=False, completa=False, maximiza=False)
-buscar_nota = Ventana.Ventana(width=600, height=400, titulo="Anotador - Buscar Notas", root=False, completa=False, maximiza=False)
-listar_notas = Ventana.Ventana(width=600, height=400, titulo="Anotador - Listar Notas", root=False, completa=False, maximiza=False)
+root = Ventana.Ventana(width=400, height=300, titulo="Anotador - Login", root=True, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
+main = Ventana.Ventana(width=400, height=300, titulo="Anotador - Main", root=True, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
+crear_nota = Ventana.Ventana(width=400, height=600, titulo="Anotador - Crear Nota", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
+modificar_nota = Ventana.Ventana(width=400, height=600, titulo="Anotador - Modificar Nota", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
+nota = Ventana.Ventana(width=330, height=330, titulo="Anotador - Nota", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
+buscar_nota = Ventana.Ventana(width=600, height=400, titulo="Anotador - Buscar Notas", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
+listar_notas = Ventana.Ventana(width=600, height=400, titulo="Anotador - Listar Notas", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
 
 # CREAR LA VENTANA
 root.crear_ventana()    
