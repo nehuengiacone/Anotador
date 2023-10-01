a50746009ff89633d0ccdceb86d19afd9e9f4bb0
Nehuen Giacone <nehuengiacone@gmail.com>
2023-09-30 23:06:26 -0300

    cambios entrantes sin aceptar

--- view/windows/windows_definitions.py
+++ view/windows/windows_definitions.py
@@ -6,13 +6,6 @@ from classes import Ventana
 
 # INSTANCIAS DE VENTANA
 #instancia de ventana principal
-<<<<<<< HEAD
-root = classes.Ventana.Ventana(width=500, height=800, completa=False, maximiza=False)
-
-
-
-
-=======
 root = Ventana.Ventana(width=400, height=300, titulo="Anotador - Login", root=True, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
 crear_cuenta = Ventana.Ventana(width=400, height=300, titulo="Anotador - Crear Cuenta", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
 main = Ventana.Ventana(width=400, height=300, titulo="Anotador - Main", root=True, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
@@ -21,7 +14,6 @@ modificar_nota = Ventana.Ventana(width=400, height=600, titulo="Anotador - Modif
 nota = Ventana.Ventana(width=330, height=330, titulo="Anotador - Nota", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
 buscar_nota = Ventana.Ventana(width=600, height=400, titulo="Anotador - Buscar Notas", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
 listar_notas = Ventana.Ventana(width=600, height=400, titulo="Anotador - Listar Notas", root=False, completa=False, maximiza=False, background_color="lightyellow", icono="assets/imgs/icono.ico")
->>>>>>> desarrollo
 
 # CREAR LA VENTANA
 root.crear_ventana()    
